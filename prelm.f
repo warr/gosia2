a3014ba1fe93db6795f396921f2cdbd8cc4c498e
Nigel Warr <warr@ikp.uni-koeln.de>
2007-07-18 15:00:22 +0200

    Add option 4 to prelm.f and handle case of ELM25/6

--- prelm.f
+++ prelm.f
@@ -9,10 +9,13 @@ C Purpose: print matrix elements
 C
 C Uses global variables:
 C      ELM    - matrix elements
+C      ELM25  - storage for matrix elements for target (added for gosia2)
+C      ELM26  - storage for matrix elements for projectile (added for gosia2)
 C      ELML   - lower limits on matrix elements
 C      ELMU   - upper limits on matrix elements
 C      IFAC   -
 C      IVAR   - indicates a limit or correlation is set
+C      JZB    - unit to read from (added for gosia2)
 C      LDNUM  - number of matrix elements with each multipolarity populating levels
 C      LEAD   - pair of levels involved in each matrix element
 C      MULTI  - number of matrix elements having a given multipolarity
@@ -24,14 +27,15 @@ C      Iop    - print flag (controls what is written to output).
  
       SUBROUTINE PRELM(Iop)
       IMPLICIT NONE
-      REAL*8 ACCA , ACCUR , b , DIPOL , ELM , ELML , ELMU , EN , HLM , 
+      REAL*8 ACCA , ACCUR , b , DIPOL , ELM , ELML , ELMU , EN ,
      &       pv , SA , SPIN , ste , ZPOL
+      REAL*8 ELM25 , ELM26 ! Added for gosia2
       INTEGER*4 inx , Iop , ISO , isp , IVAR , j , k , kk , l , LAMDA , 
      &          LAMMAX , LDNUM , LEAD , LMAXE , m , MAGEXC , MEMAX , 
      &          MEMX6 , MULTI , NDIM
       INTEGER*4 NMAX , NMAX1
+      INTEGER*4 IBPS , JZB ! Added for gosia2
       CHARACTER*3 wrn
-      COMMON /HHH   / HLM(500)
       COMMON /COMME / ELM(500) , ELMU(500) , ELML(500) , SA(500)
       COMMON /CEXC  / MAGEXC , MEMAX , LMAXE , MEMX6 , IVAR(500)
       COMMON /COEX  / EN(75) , SPIN(75) , ACCUR , DIPOL , ZPOL , ACCA , 
@@ -39,10 +43,13 @@ C      Iop    - print flag (controls what is written to output).
       COMMON /CLCOM / LAMDA(8) , LEAD(2,500) , LDNUM(8,75) , LAMMAX , 
      &                MULTI(8)
       COMMON /COEX2 / NMAX , NDIM , NMAX1
+      COMMON /SWITCH/ JZB , IBPS ! Added for gosia2
+      COMMON /RESC  / ELM25(500) , ELM26(500)
 
       inx = 0
       WRITE (22,99001)
 99001 FORMAT (2X/40X,'MATRIX ELEMENTS',//)
+
       DO j = 1 , 8
          m = MULTI(j)
          IF ( m.NE.0 ) THEN
@@ -50,11 +57,12 @@ C      Iop    - print flag (controls what is written to output).
 99002       FORMAT (5X,'MULTIPOLARITY=',1I1)
             IF ( Iop.EQ.1 ) WRITE (22,99003)
 99003       FORMAT (4X,'INDEX',3X,'NF',5X,'NS',10X,'ME')
-            IF ( Iop.EQ.2 ) WRITE (22,99004)
+            IF ( Iop.EQ.2 .OR. Iop.EQ.4 ) WRITE (22,99004)
 99004       FORMAT (4X,'INDEX',3X,'NF',5X,'NS',10X,'ME',15X,'LIMITS')
             IF ( Iop.EQ.3 ) WRITE (22,99005)
 99005       FORMAT (4X,'INDEX',3X,'NF',5X,'NS',10X,'ME',10X,'PC CHANGE',
      &              5X,'RED. TRANS. PROB.')
+
             DO k = 1 , NMAX
                l = LDNUM(j,k)
                IF ( l.NE.0 ) THEN
@@ -64,14 +72,10 @@ C      Iop    - print flag (controls what is written to output).
                         IF ( IVAR(inx).EQ.0 ) THEN ! Fixed
                            WRITE (22,99006) inx , LEAD(1,inx) , 
      &                            LEAD(2,inx) , ELM(inx)
-99006                      FORMAT (5X,1I3,5X,1I2,5X,1I2,5X,1F10.5,5X,
-     &                             'FIXED')
                         ELSEIF ( IVAR(inx).GT.1000 ) THEN ! Correlation
                            WRITE (22,99007) inx , LEAD(1,inx) , 
      &                            LEAD(2,inx) , ELM(inx) , 
      &                            (IVAR(inx)-1000)
-99007                      FORMAT (5X,1I3,5X,1I2,5X,1I2,5X,1F10.5,5X,
-     &                             'COUPLED TO',1X,1I3)
                         ELSE ! Limit
                            WRITE (22,99009) inx , LEAD(1,inx) , 
      &                            LEAD(2,inx) , ELM(inx) , ELML(inx) , 
@@ -83,21 +87,45 @@ C      Iop    - print flag (controls what is written to output).
                         wrn = '   '
                         IF ( (ELM(inx)-ELML(inx)).LT.pv ) wrn = '*?*'
                         IF ( (ELMU(inx)-ELM(inx)).LT.pv ) wrn = '*?*'
-                        ste = HLM(inx)
+                        IF ( JZB.EQ.25 ) ste = ELM25(inx)
+                        IF ( JZB.EQ.26 ) ste = ELM26(inx)
                         b = ELM(inx)*ELM(inx)/(2.*SPIN(isp)+1.)
                         IF ( LEAD(1,inx).EQ.LEAD(2,inx) ) b = 9999999.
                         WRITE (22,99009) inx , LEAD(1,inx) , LEAD(2,inx)
      &                         , ELM(inx) , 100.*(ELM(inx)-ste)/ste , 
      &                         b , wrn
+                     ELSEIF ( Iop.EQ.4 ) THEN
+                        IF ( IVAR(inx).EQ.0 ) THEN ! Fixed
+                           WRITE (22,99006) inx , LEAD(1,inx) , 
+     &                            LEAD(2,inx) , ELM(inx)
+                        ELSEIF ( IVAR(inx).GT.1000 ) THEN ! Correlation
+                           WRITE (22,99007) inx , LEAD(1,inx) , 
+     &                            LEAD(2,inx) , ELM(inx) , 
+     &                            (IVAR(inx)-1000)
+                        ELSE ! Limit
+                           IF ( JZB.EQ.25 ) THEN
+                             WRITE (22,99009) inx , LEAD(1,inx) , 
+     &                           LEAD(2,inx) , ELM25(inx) , ELML(inx) ,
+     &                           ELMU(inx)
+                           ELSEIF ( JZB.EQ.26 ) THEN
+                             WRITE (22,99009) inx , LEAD(1,inx) , 
+     &                           LEAD(2,inx) , ELM26(inx) , ELML(inx) , 
+     &                           ELMU(inx)
+                           ENDIF
+                       ENDIF
                      ELSE
                         WRITE (22,99008) inx , LEAD(1,inx) , LEAD(2,inx)
      &                         , ELM(inx)
-99008                   FORMAT (5X,1I3,5X,1I2,5X,1I2,5X,1F10.5)
                      ENDIF
-                  ENDDO
-               ENDIF
-            ENDDO
-         ENDIF
-      ENDDO
+                  ENDDO ! Loop on kk
+               ENDIF ! If l .ne. 0
+            ENDDO ! Loop on k
+
+         ENDIF ! If m .ne. 0
+      ENDDO ! Loop on j
+
+99006 FORMAT (5X,1I3,5X,1I2,5X,1I2,5X,1F10.5,5X,'FIXED')
+99007 FORMAT (5X,1I3,5X,1I2,5X,1I2,5X,1F10.5,5X,'COUPLED TO',1X,1I3)
+99008 FORMAT (5X,1I3,5X,1I2,5X,1I2,5X,1F10.5)
 99009 FORMAT (5X,1I3,5X,1I2,5X,1I2,3(5X,1F10.5),1A3)
       END
