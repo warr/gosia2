b9a582cf094251532d3ae9fb13f00565e72a4302
Nigel Warr <warr@ikp.uni-koeln.de>
2007-07-19 10:11:57 +0200

    Add a comment that was already added to gosia.f

--- gosia2.f
+++ gosia2.f
@@ -84,7 +84,7 @@ C      CNOR2  - normalization factors for projectile (for gosia2)
 C      CORF   - internal correction factors
 C      DEVD   -
 C      DEVU   -
-C      DIPOL  - 
+C      DIPOL  - E1 polarization parameter
 C      DIX    - Ge parameters (inner & outer radius, length, distance)
 C      DLOCK  - limit derivative below which matrix element is fixed if LOCKS=1
 C      DS     -
