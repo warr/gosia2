89a9f9e00641d385674eb7d19290bfda02236e35
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 10:54:52 +0200

    Comments in FIINT1

--- fiint1.f
+++ fiint1.f
@@ -1,8 +1,23 @@
+ 
+C----------------------------------------------------------------------
+C SUBROUTINE FIINT1
+C
+C Called by: ANGULA
+C
+C Purpose: integrate over phi in lab frame
+C
+C Formal parameters:
+C      Fi0    - phi_0
+C      Fi1    - phi_1
+C      Alab   -
+C      Ixs    - axial symmetry flag
+
       SUBROUTINE FIINT1(Fi0,Fi1,Alab,Ixs)
       IMPLICIT NONE
       REAL*8 Alab , Fi0 , Fi1 , wsp
       INTEGER*4 Ixs , j , m , mm
       DIMENSION Alab(9,9)
+      
       IF ( Ixs.NE.0 ) THEN
          DO m = 2 , 9
             mm = m - 1
@@ -13,7 +28,7 @@
          ENDDO
          wsp = Fi1 - Fi0
       ENDIF
-      IF ( Ixs.EQ.0 ) wsp = 6.283185308
+      IF ( Ixs.EQ.0 ) wsp = 6.283185308 ! 6.283185308 = 2 * pi
       DO j = 1 , 9
          Alab(j,1) = Alab(j,1)*wsp
       ENDDO
