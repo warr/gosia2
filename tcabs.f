5f3e1607e894f0480469a4a830525dbbfb34d004
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 11:26:49 +0200

    Comments in TCABS

--- tcabs.f
+++ tcabs.f
@@ -1,7 +1,22 @@
+ 
+C----------------------------------------------------------------------
+C FUNCTION TCABS
+C
+C Called by: laiamp, pomnoz
+C
+C Purpose: evaluates the absolute value of a complex number
+C
+C Formal parameters:
+C      Z      - argument for abs
+C
+C Return value:
+C      absolute value of complex number Z.
+ 
       REAL*8 FUNCTION TCABS(Z)
       IMPLICIT NONE
       REAL*8 a , b
       COMPLEX*16 Z
+      
       a = DBLE(Z)
       b = IMAG(Z)
       IF ( ABS(a).LT.1.E-16 ) a = 0.
