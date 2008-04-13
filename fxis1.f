c38b9be4dfb4df664fdedbaf2d2edb439e207584
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 11:01:45 +0200

    Make fxis1 explicitly REAL*8 FUNCTION

--- fxis1.f
+++ fxis1.f
@@ -1,7 +1,7 @@
-      FUNCTION FXIS1(I,N)
+      REAL*8 FUNCTION FXIS1(I,N)
       IMPLICIT NONE
-      REAL*8 FXIS1 , XI
       INTEGER*4 I , N
+      REAL*8 XI
       COMMON /CXI   / XI(500)
       IF ( N.EQ.2 .OR. N.EQ.3 .OR. N.EQ.5 .OR. N.EQ.6 ) THEN
          FXIS1 = 1.
