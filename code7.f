3c62e9abd7bb9d44c1c25e27c0d289181c534003
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 10:21:57 +0200

    Comments in CODE7

--- code7.f
+++ code7.f
@@ -1,3 +1,23 @@
+ 
+C----------------------------------------------------------------------
+C SUBROUTINE CODE7
+C
+C Called by: LSLOOP
+C
+C Purpose:
+C
+C Uses global variables:
+C      IAPR   -
+C      IPATH  -
+C
+C Formal parameters:
+C      Ir     -
+C      Is     -
+C      N      -
+C      Mt     -
+C      Inqa   -
+C      Indx   -
+ 
       SUBROUTINE CODE7(Ir,Is,N,Mt,Inqa,Indx)
       IMPLICIT NONE
       INTEGER*4 IAPR , idm , idn , Indx , Inqa , IPATH , Ir , Is , 
@@ -5,6 +25,7 @@
       REAL*8 QAPR
       COMMON /PTH   / IPATH(75) , MAGA(75)
       COMMON /APRCAT/ QAPR(500,2,7) , IAPR(500,2) , ISEX(75)
+      
       IAPR(Indx,1) = N
       IAPR(Indx,2) = Mt
       IF ( IPATH(N).EQ.0 .OR. IPATH(Mt).EQ.0 ) THEN
