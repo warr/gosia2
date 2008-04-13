e516a2d056adfe3444e4da021c381f3c7f3c7ae7
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 11:29:55 +0200

    Comments in TACOS

--- tacos.f
+++ tacos.f
@@ -1,5 +1,23 @@
+ 
+C----------------------------------------------------------------------
+C FUNCTION TACOS
+C
+C Called by: ARCCOS, CEGRY, COORD, GOSIA2
+C Calls:     TASIN
+C
+C Purpose: evaluate arccosine(x)
+C
+C Formal parameters:
+C      X      - value for which we are to evaluate the arccosine
+C
+C Return value:
+C      arccosine of X
+C
+C We use: arccos(x) = pi/2 - arcsin(x)
+ 
       REAL*8 FUNCTION TACOS(X)
       IMPLICIT NONE
       REAL*8 TASIN , X
-      TACOS = 1.570796327 - TASIN(X)
+      
+      TACOS = 1.570796327 - TASIN(X) ! 1.570796327 = pi / 2
       END
