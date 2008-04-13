9b79006820919a5eafac99f79875aa3633a8c352
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 11:04:00 +0200

    Comments in FXIS2

--- fxis2.f
+++ fxis2.f
@@ -1,8 +1,27 @@
+ 
+C----------------------------------------------------------------------
+C FUNCTION FXIS2
+C
+C Called by: NEWCAT
+C
+C Purpose: return -1 * sign(xi) for N = 2,3,5 and 6
+C
+C Uses global variables:
+C      XI     - xi coupling coefficients
+C
+C Formal parameters:
+C      I      - index into XI array
+C      N      - 
+C
+C Return value:
+C      sign of xi
+
       REAL*8 FUNCTION FXIS2(I,N)
       IMPLICIT NONE
       INTEGER*4 I , N
       REAL*8 XI
       COMMON /CXI   / XI(500)
+
       IF ( N.EQ.2 .OR. N.EQ.3 .OR. N.EQ.5 .OR. N.EQ.6 ) THEN
          FXIS2 = -SIGN(1.,XI(I))
          GOTO 99999
