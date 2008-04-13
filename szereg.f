0a20c3407b921de3ac09a9592343a8a2eacfe133
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 11:36:47 +0200

    Comments in SZEREG

--- szereg.f
+++ szereg.f
@@ -1,3 +1,21 @@
+ 
+C----------------------------------------------------------------------
+C SUBROUTINE SZEREG
+C
+C Called by: READY
+C
+C Purpose: sort out the decay sequence
+C
+C Uses global variables:
+C      DYEX   - error on experimental yield
+C      IY     - index for yields
+C      YEXP   - experimental yield
+C
+C Formal parameters:
+C      Lst    - start
+C      Ls     - end
+C      L      -
+ 
       SUBROUTINE SZEREG(Lst,Ls,L)
       IMPLICIT NONE
       REAL*8 CORF , DYEX , dyh , UPL , YEXP , yh , YNRM
@@ -6,11 +24,14 @@
       COMMON /YEXPT / YEXP(32,1500) , IY(1500,32) , CORF(1500,32) , 
      &                DYEX(32,1500) , NYLDE(50,32) , UPL(32,50) , 
      &                YNRM(32,50) , IDRN , ILE(32)
+
       IF ( Lst.EQ.Ls ) RETURN
       lst1 = Lst
       lsp = Ls - 1
+
  100  ia = IY(lst1,L)
       IF ( ia.GT.1000 ) ia = ia/1000
+
       inx = lst1
       DO k = lst1 , lsp
          ib = IY(k+1,L)
@@ -18,6 +39,8 @@
          ia = MIN(ia,ib)
          IF ( ia.EQ.ib ) inx = k + 1
       ENDDO
+
+C     Swap them
       IF ( inx.NE.lst1 ) THEN
          ih = IY(lst1,L)
          IY(lst1,L) = IY(inx,L)
@@ -29,6 +52,7 @@
          YEXP(L,inx) = yh
          DYEX(L,inx) = dyh
       ENDIF
+
       lst1 = lst1 + 1
       IF ( lst1.GT.lsp ) RETURN
       GOTO 100
