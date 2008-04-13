d1c4c2fe4bf13af25192bc4b8dd75820ab8d3ffa
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-13 11:24:35 +0200

    Comments in TCEXP

--- tcexp.f
+++ tcexp.f
@@ -1,7 +1,22 @@
+ 
+C----------------------------------------------------------------------
+C FUNCTION TCEXP
+C
+C Called by: EXPON
+C
+C Purpose: evaluates a complex exponential
+C
+C Formal parameters:
+C      Z      - argument of exponential (complex)
+C
+C Return value:
+C      complex exponential of Z.
+ 
       COMPLEX*16 FUNCTION TCEXP(Z)
       IMPLICIT NONE
       REAL*8 a , b , c , d
       COMPLEX*16 Z
+      
       a = DBLE(Z)
       b = IMAG(Z)
       a = EXP(a)
