abf94fe039fd4874ea9fc3cb188cc1bd6b2d705e
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-14 06:59:47 +0200

    Comments in RNDM

--- rndm.f
+++ rndm.f
@@ -1,7 +1,22 @@
+ 
+C----------------------------------------------------------------------
+C FUNCTION RNDM
+C
+C Called by: MIXUP
+C
+C Purpose: Generate a pseudo-random number based on the seed Se
+C
+C Formal parameters:
+C      Se     - seed for random number
+C
+C It is used to generate random matrix elements as a starting position,
+C when OP,RAND is called. The parameter to OP,RAND is the seed here.
+ 
       REAL*8 FUNCTION RNDM(Se)
       IMPLICIT NONE
       REAL*8 ai , p , r , rxdm , Se , t , u
       INTEGER*4 i
+
       IF ( Se.GT.32000. ) Se = 100.*t + .511
       Se = Se*Se
       u = LOG10(Se)
