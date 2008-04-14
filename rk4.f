5d88648f455416f7dcccd791107dddee388c0277
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-14 07:00:32 +0200

    Comments in RK4

--- rk4.f
+++ rk4.f
@@ -1,6 +1,22 @@
+ 
+C----------------------------------------------------------------------
+C FUNCTION RK4
+C
+C Called by: KONTUR
+C
+C Purpose:
+C
+C Formal parameters:
+C      Y      - 
+C      H      -
+C      F      - array of three coefficients
+C
+C Return value:
+ 
       REAL*8 FUNCTION RK4(Y,H,F)
       IMPLICIT NONE
       REAL*8 F , H , Y
       DIMENSION F(3)
+
       RK4 = Y + H*(F(1)+4.*F(2)+F(3))/6.
       END
