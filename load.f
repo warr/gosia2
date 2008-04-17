bf60b6b0806b5b533d6240c0b8daca3a8d0c4aeb
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-17 14:20:37 +0200

    Comments in load.f

--- load.f
+++ load.f
@@ -10,39 +10,39 @@ C variables XI (common CXI) and PSI (common PCOM).
 C
 C Uses global variables:
 C      CAT    - substates of levels (n_level, J, m)
-C      DIPOL  -
+C      DIPOL  - E1 polarization parameter
 C      EMMA   - Controls number of magnetic substates in full coulex calc.
 C      EN     - energy of level
 C      EP     - bombarding energy
-C      ERR    -
-C      IFAC   -
-C      IPATH  -
-C      ISHA   -
-C      ISMAX  -
+C      ERR    - error flag
+C      IPATH  - index of substate in level with same m as substate Irld
+C      ISHA   - is half-integer spin
+C      ISMAX  - number of substates used
 C      IZ     - Z of investigated nucleus
 C      IZ1    - Z of not-investated nucleus
 C      LAMDA  - list of multipolarities to calculate
 C      LAMMAX - number of multipolarities to calculate
 C      LDNUM  - number of matrix elements with each multipolarity populating level
 C      LEAD   - pair of levels involved in each matrix element
-C      LMAX   -
+C      LMAX   - ground-state spin + 1
 C      LMAXE  - maximum multipolarity needed for calculation
 C      LP7    - maximum number of zeta coefficients (45100)
-C      LP10   - 600
+C      LP10   - maximum number of reduced matrix elements (600)
 C      LZETA  - index into ZETA array for zeta for a given multipolarity
 C      MAGA   - number of magnetic substates in approximate calculation
-C      MAGEXC -
+C      MAGEXC - flag: 0 means no magnetic excitations, 1 means with mag. exc.
 C      MEMAX  - number of matrix elements
-C      NSTART -
-C      NSTOP  -
+C      NMAX   - number of levels
+C      NSTART - index in CAT of first substate associated with a level
+C      NSTOP  - index in CAT of last substate associated with a level
 C      PSI    - psi coefficients
-C      QAPR   -
+C      QAPR   - approximate Coulomb amplitudes
 C      SPIN   - spin of level
 C      VINF   - speed of projectile at infinity
 C      XA     - A of investigated nucleus
 C      XA1    - A of not-investated nucleus
 C      XI     - xi coupling constants
-C      ZPOL   -
+C      ZPOL   - dipole term
 C
 C Formal parameters:
 C      Iexp   - Number of experiment
@@ -93,12 +93,12 @@ C      Joj    - index of substate (write only)
       COMMON /PTH   / IPATH(75) , MAGA(75)
       DIMENSION etan(75) , cpsi(8)
       
-      LMAX = INT(SPIN(1)+1.1)
+      LMAX = INT(SPIN(1)+1.1) ! ground-state spin + 1
       IF ( Ient.EQ.1 ) THEN
          ISHA = 0
          ispi = INT(SPIN(1)+.51)
          ispo = INT(SPIN(1)+.49)
-         IF ( ispi.NE.ispo ) ISHA = 1
+         IF ( ispi.NE.ispo ) ISHA = 1 ! Half-integer spin
          z1 = DBLE(ABS(IZ1(Iexp)))
          z2 = DBLE(IZ)
          a1 = XA1(Iexp)
@@ -131,6 +131,8 @@ C        i.e. hbar c / e^2 * sqrt(2 / amu).
 C        Calculate C_\lambda \over (s Z_1 Z_2)^\lambda 
          aazz = 1./(1.+a1/a2)/z1/z2
          cpsi(1) = 5.169286*aazz
+
+C        Electric excitations up to LMAXE
          IF ( LMAXE.NE.1 ) THEN
             aaz2 = aazz*aazz
             cpsi(2) = 14.359366*aaz2
@@ -151,7 +153,9 @@ C        Calculate C_\lambda \over (s Z_1 Z_2)^\lambda
                ENDIF
             ENDIF
          ENDIF
-         IF ( MAGEXC.NE.0 ) THEN
+
+C        Magnetic excitations
+         IF ( MAGEXC.NE.0 ) THEN ! If magnetic excitations are required
             aazz = VINF(Iexp)/95.0981942
             cpsi(7) = aazz*cpsi(1)
             IF ( LAMMAX.NE.8 ) cpsi(8) = aazz*cpsi(2)
@@ -161,16 +165,16 @@ C        Calculate psi and store in PSI in common PCOM
          zsqa = z1*SQRT(a1)
          i3 = 1
          ppp = 1. + a1/a2
-         DO i1 = 1 , LAMMAX
+         DO i1 = 1 , LAMMAX ! For each calculated multipolarity
             lam = LAMDA(i1)
             lam1 = lam
             IF ( lam.GT.6 ) lam1 = lam - 6
-            DO n2 = 1 , NMAX
-               nn = LDNUM(lam,n2)
+            DO n2 = 1 , NMAX ! For each level
+               nn = LDNUM(lam,n2) ! Number of matrix elements connected to this level by this multipolarity
                IF ( nn.NE.0 ) THEN
                   n3 = LEAD(1,i3)
                   pp1 = EP(Iexp) - ppp*EN(n3)
-                  DO m1 = 1 , nn
+                  DO m1 = 1 , nn ! For each matrix element connected to level with this multipolarity
                      m2 = LEAD(2,i3)
                      i2 = i3
                      i3 = i3 + 1
@@ -185,42 +189,42 @@ C        Calculate psi and store in PSI in common PCOM
       ENDIF
 
 C     Initialise NSTART and NSTOP arrays
-      DO n = 1 , NMAX
+      DO n = 1 , NMAX ! For each level
          NSTART(n) = 0
          NSTOP(n) = 0
       ENDDO
 
       is = 1
       NSTART(1) = 1
-      DO n = 1 , NMAX
+      DO n = 1 , NMAX ! For each level
          wrt = Polm - EMMA(Iexp)
          wrtm = Polm + EMMA(Iexp)
          IF ( Icg.EQ.2 ) wrt = Polm - DBLE(MAGA(Iexp))
          IF ( Icg.EQ.2 ) wrtm = Polm + DBLE(MAGA(Iexp))
          IF ( wrtm.LT.-SPIN(n) ) THEN
-            NSTART(n) = 0
+            NSTART(n) = 0 ! Level has no interesting substates
          ELSE
             IF ( ABS(wrt).GT.SPIN(n) ) wrt = -SPIN(n)
             IF ( wrtm.GT.SPIN(n) ) wrtm = SPIN(n)
             mstop = INT(wrtm-wrt+1.01)
-            DO i = 1 , mstop
+            DO i = 1 , mstop ! For each substate
                CAT(is,1) = n               ! Number of level
                CAT(is,2) = SPIN(n)         ! Spin of level
                CAT(is,3) = wrt + DBLE(i-1) ! m quantum number of substate
                IF ( n.EQ.1 .AND. ABS(CAT(is,3)-Polm).LT.1.E-6 ) Joj = is
                is = is + 1
-            ENDDO
+            ENDDO ! Loop on substates i
          ENDIF
-         NSTART(n+1) = is
-         NSTOP(n) = is - 1
-      ENDDO
+         NSTART(n+1) = is ! First substate of level n+1
+         NSTOP(n) = is - 1 ! Last substate of level n
+      ENDDO ! Loop on levels n
 
-      ISMAX = is - 1
+      ISMAX = is - 1 ! ISMAX is the number of substates used
       IF ( ISMAX.LE.LP10 ) THEN ! LP10 is max. number of substates (600)
          IF ( Ient.EQ.3 ) RETURN
          nz = 0
          DO jj = 1 , 7
-            DO jjj = 1 , MEMAX
+            DO jjj = 1 , MEMAX ! For each matrix element
                QAPR(jjj,1,jj) = 0.
                QAPR(jjj,2,jj) = 0.
             ENDDO
@@ -231,7 +235,7 @@ C        Initialise pointers to ZETA array
             LZETA(i) = 0
          ENDDO
 
-         DO i1 = 1 , LAMMAX
+         DO i1 = 1 , LAMMAX ! For each multipolarity
             lam = LAMDA(i1)
             IF ( Icg.NE.2 .OR. lam.LE.6 ) THEN
                la = lam
@@ -247,7 +251,7 @@ C        Initialise pointers to ZETA array
                      IF ( MAGA(Iexp).EQ.0 .AND. ir.NE.IPATH(n) ) GOTO 10
                      IF ( ABS(ir-IPATH(n)).GT.1 ) GOTO 10
                   ENDIF
-                  ld = LDNUM(la,n)
+                  ld = LDNUM(la,n) ! Number of matrix elements connected to this level by this multipolarity
                   IF ( ld.EQ.0 ) THEN
                      ir = ir + NSTOP(n) - NSTART(n)
                   ELSE
@@ -257,8 +261,9 @@ C        Initialise pointers to ZETA array
                ENDIF
             ENDIF
          ENDDO ! Loop over multipolarity
-          
-         IF ( nz.GT.LP7 ) THEN
+
+C        Make sure we haven't reached the limit of zeta coefficients          
+         IF ( nz.GT.LP7 ) THEN ! LP7 is 45100
             WRITE (22,99001) LP7
 99001       FORMAT (1x,
      &              'ERROR - NUMBER OF ELEMENTS IN ZETA ARRAY EXCEEDS',
@@ -271,5 +276,5 @@ C        Initialise pointers to ZETA array
 99002    FORMAT (' ERROR-ISMAX EXCEEDS MAGMAX',5X,'(MAGMAX =',I4,')')
       ENDIF
 
-      ERR = .TRUE.
+      ERR = .TRUE. ! Set error flag
       END
