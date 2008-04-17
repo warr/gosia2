d9705e3752569b62fa7a038a4b0348225eca3c57
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-17 14:12:03 +0200

    Comments in reset.f

--- reset.f
+++ reset.f
@@ -9,14 +9,14 @@ C          of f(n-2), f(n-2) is set to the old value of f(n-1) and f(n-1) is
 C          set to the old value of f(n).
 C
 C Uses global variables:
-C      ARM    - reduced matrix elements
+C      ARM    - excitation amplitudes of substates.
 C      CAT    - substates of levels (n_level, J, m)
-C      ISMAX  -
+C      ISMAX  - number of substates used
 C      NMAX   - number of levels
-C      NSTART -
+C      NSTART - index in CAT of first substate associated with a level
 C
 C Formal parameters:
-C      Iso    -
+C      Iso    - isotropic flag
  
       SUBROUTINE RESET(Iso)
       IMPLICIT NONE
@@ -30,8 +30,8 @@ C      Iso    -
       COMMON /COEX2 / NMAX , NDIM , NMAX1
       
       IF ( Iso.EQ.0 ) THEN
-         DO j = 1 , NMAX
-            ir = NSTART(j) - 1
+         DO j = 1 , NMAX ! Loop over levels
+            ir = NSTART(j) - 1 ! Index of first substate of level - 1
  20         ir = ir + 1
             ARM(ir,1) = ARM(ir,2)
             ARM(ir,2) = ARM(ir,3)
@@ -41,7 +41,7 @@ C      Iso    -
          GOTO 99999
       ENDIF
        
-      DO j = 1 , ISMAX
+      DO j = 1 , ISMAX ! Loop over substates
          ARM(j,1) = ARM(j,2)
          ARM(j,2) = ARM(j,3)
          ARM(j,3) = ARM(j,4)
