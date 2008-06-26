d0ff85218a8d88e6b909dc466cf941cc4892691f
Nigel Warr <warr@ikp.uni-koeln.de>
2008-06-26 08:30:51 +0200

    Add script to make archives

---
+++ make_archives.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+list=$(git tag -l |grep ^200)
+
+for t in $list ; do
+    echo "Creating archive for gosia2 $t"
+    git archive --format=tar --prefix=gosia2_$t/ $t | gzip -9 > /tmp/gosia2_$t.tgz
+done
