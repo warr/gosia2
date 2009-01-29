1de477f09ce075794799fe47297dee29601909c6
Nigel Warr <warr@ikp.uni-koeln.de>
2009-01-29 07:53:39 +0100

    Fix make_archives.sh

--- make_archives.sh
+++ make_archives.sh
@@ -4,5 +4,5 @@ list=$(git tag -l |grep ^200)
 
 for t in $list ; do
     echo "Creating archive for gosia2 $t"
-    git archive --format=tar --prefix=gosia2_$t/ $t | gzip -9 > /tmp/gosia2_$t.tgz
+    git archive --format=tar --prefix=gosia2-$t/ $t | gzip -9 > /tmp/gosia2_$t.tgz
 done
