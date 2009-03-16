e171b8fd2b44111c09c515cbe786995616fdd563
Nigel Warr <warr@ikp.uni-koeln.de>
2009-03-16 14:15:58 +0100

    Remove make_archives.sh

--- make_archives.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-list=$(git tag -l |grep ^200)
-
-for t in $list ; do
-    echo "Creating archive for gosia2 $t"
-    git archive --format=tar --prefix=gosia2-$t/ $t | gzip -9 > /tmp/gosia2_$t.tgz
-done
