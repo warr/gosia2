5557ef13b117cf96e2928f829418eb6abc33f113
Nigel Warr <warr@ikp.uni-koeln.de>
2010-04-23 09:48:07 +0200

    Fix LCK, bug

--- mini.f
+++ mini.f
@@ -107,7 +107,7 @@ C     Initialise gradp to zero for each matrix element
          gradp(i) = 0.
       ENDDO
 
-C     Initialise some parameters to zero
+C     Initialise some parameters
       icount = 0
       lnm = 0
       LNY = 0
@@ -116,6 +116,7 @@ C     Initialise some parameters to zero
       LFL1 = 0
       ncall = 0
       ITAK2 = 0
+      chil = 1.D38
 
 C     Handle the different modes
 C     Imode = IJKL, where
