73dc34c924f7b8941bea5777b8588c7a92ae9486
Nigel Warr <warr@ikp.uni-koeln.de>
2018-05-29 09:15:29 +0200

    Fix warning in metacode

--- include.c
+++ include.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+int treat_line(char *line);
+int treat_file(char *filename);
+
 /*****************************************************************************/
 int treat_line(char *line) {
 
